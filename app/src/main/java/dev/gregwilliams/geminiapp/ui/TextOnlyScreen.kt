ff869252fa894dd0ef79981db4f5ea1a2413c451
Greg Williams <greg@greg-williams.com>
2024-04-03 19:53:54 -0400

    longpress the response to copy to the clipboard

--- app/src/main/java/dev/gregwilliams/geminiapp/ui/TextOnlyScreen.kt
+++ app/src/main/java/dev/gregwilliams/geminiapp/ui/TextOnlyScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
@@ -26,7 +27,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -34,6 +37,7 @@ import dev.gregwilliams.geminiapp.R
 import dev.gregwilliams.geminiapp.textexample.TextOnlyViewModel
 import dev.gregwilliams.geminiapp.ui.theme.GeminiApplicationTheme
 import dev.gregwilliams.geminiapp.util.BooleanPreviewParamProvider
+import dev.gregwilliams.geminiapp.util.longPressToCopyToClipboard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -86,7 +90,8 @@ private fun TextOnlyQueryView(
                 .fillMaxWidth(),
             value = inputText,
             onValueChange = onInputChange,
-            minLines = 2
+            minLines = 2,
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences)
         )
         Button(
             modifier = Modifier.width(170.dp),
@@ -102,10 +107,12 @@ private fun TextOnlyQueryView(
             }
         }
 
+        val context = LocalContext.current
         Text(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 16.dp),
+                .padding(top = 16.dp)
+                .longPressToCopyToClipboard(context, response),
             color = MaterialTheme.colorScheme.onBackground,
             text = response
         )
